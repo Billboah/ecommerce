ad3fbbeae3dd69038da994af3102dca53f141ce2
Yeboah William <97909844+Billboah@users.noreply.github.com>
2025-06-08 09:29:41 +0000

    commit

--- app/products/[id]/page.tsx
+++ app/products/[id]/page.tsx
@@ -4,25 +4,23 @@ import ProductDetailClient from "@/components/productDetailClient";
 import { ProductType } from "@/types";
 
 type Props = {
-  params: { id: string };
+  params: {
+    id: string;
+  };
 };
 
 export default async function ProductDetailPage({ params }: Props) {
-  let product: ProductType | null = null;
-  const { id } = await params;
-  console.log("Fetching product with ID:", id);
+  const { id } = params;
 
   try {
     const res = await axios.get<ProductType>(
       `https://fakestoreapi.com/products/${id}`
     );
-    product = res.data;
+    const product = res.data;
+
+    return <ProductDetailClient product={product} />;
   } catch (error) {
     console.error("Error fetching product:", error);
     return notFound();
   }
-
-  if (!product) return notFound();
-
-  return <ProductDetailClient product={product} />;
 }
